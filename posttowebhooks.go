4697d063a587fdd13cd75374b5c1bbfe4965767f
Sriram Srinivasan <sriram@cyverse.org>
2017-09-06 17:24:43 -0700

    define json template

--- posttowebhooks.go
+++ posttowebhooks.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
@@ -8,6 +9,11 @@ import (
 	"github.com/buger/jsonparser"
 	"github.com/streadway/amqp"
 )
+
+//Slack json stuct for Slack
+type Slack struct {
+	Text string `json:"text"`
+}
 
 //ProcessMessages process the received message for post to webhooks
 func ProcessMessages(msgs <-chan amqp.Delivery) {
@@ -26,11 +32,16 @@ func ProcessMessages(msgs <-chan amqp.Delivery) {
 }
 
 func prepareMessage(msg []byte) *strings.Reader {
-	return strings.NewReader("{\"text\":\"" + string(msg) + "\"}")
+	//return strings.NewReader("{\"text\":\"" + string(msg) + "\"}")
+	json, err := json.Marshal(Slack{Text: string(msg)})
+	if err != nil {
+		log.Printf("Json Encoding Error: %s", err)
+	}
+	return strings.NewReader(string(json))
 }
 
 func postToHook(value []byte) {
-	resp, err := http.Post("https://hooks.slack.com/services/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", "applicatio/json", prepareMessage(value))
+	resp, err := http.Post("https://hooks.slack.com/services/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", "application/json", prepareMessage(value))
 	if err != nil {
 		log.Printf("Error posting to hook %s", err)
 	}
